4f2c2ad0634a04a07d6431a4cf38d1de3e9c4a25
Nicholas Gaydos <ngaydos@gmail.com>
2018-05-31 00:31:41 -0700

    ti_race_chooser created

---
+++ ti_race_chooser.py
@@ -0,0 +1,20 @@
+import random
+
+def game_setup(players):
+    '''takes a list of players and returns a race
+    selection for each player as well as a first speaker
+    inputs: list of players
+    outputs:'''
+    races = ['l1z1x mindnet', 'federation of sol', 'yin brotherhood', 'naalu collective', 'embers of muaat', 'mentak coalition',
+    'barony of letnev', 'xxcha kingdom', 'emirates of hacan', 'nekro virus', 'winnu', 'sardakk norr', 'ghosts of creuss', 'clan of saar'
+    'universities of jol-nar', 'arborec', 'yssaril tribes'] 
+    final = {'speaker': random.choice(players)}
+    sampled_races = random.sample(races, len(players))
+    for i, v in enumerate(players):
+        final[players[i]] = sampled_races[i]
+    return final
+
+if __name__ == '__main__':
+    playercount = input("How many players? ")
+    i = list(range(int(playercount)))
+    print(game_setup(i))
