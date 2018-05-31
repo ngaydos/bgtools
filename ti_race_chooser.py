729cd6273f59ff94e02410c6ee5cd0ae2ea7bb6f
Nicholas Gaydos <ngaydos@gmail.com>
2018-05-31 00:32:19 -0700

    updated docstring in race_chooser

--- ti_race_chooser.py
+++ ti_race_chooser.py
@@ -4,7 +4,7 @@ def game_setup(players):
     '''takes a list of players and returns a race
     selection for each player as well as a first speaker
     inputs: list of players
-    outputs:'''
+    outputs: dictionary containing a race for each player as well as a starting speaker'''
     races = ['l1z1x mindnet', 'federation of sol', 'yin brotherhood', 'naalu collective', 'embers of muaat', 'mentak coalition',
     'barony of letnev', 'xxcha kingdom', 'emirates of hacan', 'nekro virus', 'winnu', 'sardakk norr', 'ghosts of creuss', 'clan of saar'
     'universities of jol-nar', 'arborec', 'yssaril tribes'] 
