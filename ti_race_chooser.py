78cb973c39f8d394bcda649f033baeddfb7c5913
Nicholas Gaydos <ngaydos@gmail.com>
2018-06-01 19:05:12 -0700

    add coma to race_chooser

--- ti_race_chooser.py
+++ ti_race_chooser.py
@@ -6,7 +6,7 @@ def game_setup(players):
     inputs: list of players
     outputs: dictionary containing a race for each player as well as a starting speaker'''
     races = ['l1z1x mindnet', 'federation of sol', 'yin brotherhood', 'naalu collective', 'embers of muaat', 'mentak coalition',
-    'barony of letnev', 'xxcha kingdom', 'emirates of hacan', 'nekro virus', 'winnu', 'sardakk norr', 'ghosts of creuss', 'clan of saar'
+    'barony of letnev', 'xxcha kingdom', 'emirates of hacan', 'nekro virus', 'winnu', 'sardakk norr', 'ghosts of creuss', 'clan of saar',
     'universities of jol-nar', 'arborec', 'yssaril tribes'] 
     final = {'speaker': random.choice(players)}
     sampled_races = random.sample(races, len(players))
