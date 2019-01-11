e0bd1da7062ee6f7894d77cc9a49d7b649bc360d
Nicholas Gaydos <ngaydos@gmail.com>
2019-01-11 02:33:40 -0800

    created arkham_weakness generator

---
+++ arkham_weakness.py
@@ -0,0 +1,6 @@
+import random
+
+weaknesses = ['indebted','internal injury','chronophobia','overzealous','drawing the sign', 'the thing that follows', 'doomed','through the gates',
+'paranoia','amnesia','dark pact','haunted','psychosis','stubborn detective','hypochondria','mob enforcer', 'silver twilight acolyte']
+
+print random.choice(weaknesses)
