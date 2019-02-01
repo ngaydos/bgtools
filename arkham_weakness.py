186a5066d8515f7e59dec9ba8589f293c28ff831
Nicholas Gaydos <ngaydos@gmail.com>
2019-02-01 09:21:12 -0800

    added weakness for circle undone

--- arkham_weakness.py
+++ arkham_weakness.py
@@ -1,6 +1,7 @@
 import random
 
 weaknesses = ['indebted','internal injury','chronophobia','overzealous','drawing the sign', 'the thing that follows', 'doomed','through the gates',
-'paranoia','amnesia','dark pact','haunted','psychosis','stubborn detective','hypochondria','mob enforcer', 'silver twilight acolyte']
+'paranoia','amnesia','dark pact','haunted','psychosis','stubborn detective','hypochondria','mob enforcer', 'silver twilight acolyte', 'the tower',
+'the 13th vision']
 
 print random.choice(weaknesses)
